06cbc8757111ecf0497a4a61942837be356fe8b2
Anna <annasukharieva0@gmail.com>
2023-04-01 20:30:26 +0300

    first commit

---
+++ dist/js/slider.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+	$('.lessons__cards').slick({
+		infinite: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 810,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					arrows: false,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 635,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false,
+					dots: true
+				}
+			}
+		]
+	});
+
+	$('.advantages__cards-mob').slick({
+		infinite: false,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: true,
+		responsive: [ {
+				breakpoint: 690,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					arrows: false,
+					dots: true
+				},
+			}
+		]
+	});
+
+
+	$('.feedback__items-mob').slick({
+		infinite: true,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		dots: false,
+		arrows: true,
+		responsive: [
+			{
+				breakpoint: 730,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			},
+		]
+	});
+
+	arrows = document.querySelectorAll('.slick-arrow')
+	arrows.forEach(element => {
+		element.innerHTML = `<img src="icons/questions_button.svg" alt="button"></img>`;
+		element.classList.add('arrow-button')
+		if (element.getAttribute("aria-label") == 'Previous') {
+			element.firstChild.style.transform = 'rotate(-135deg)';
+		}
+		if (element.getAttribute("aria-label") == 'Next') {
+			element.firstChild.style.transform = 'rotate(45deg)';
+		}
+	});
+
+});
+
+
---
+++ dist/telegram.php
@@ -0,0 +1,27 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$token = "6184274522:AAHFWPhhkwBoSvCAp3yG8K80rcs11Wbx0j8";
+$chat_id = "-928217072";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  return true;
+} else {
+   return false;
+}
+
+?>
